44f3bbaf2d536caaade90bfd0a45b77f49b90233
Brandon07 <brandongiraldo8@gmail.com>
2026-06-20 09:46:22 -0500

    agregan nuevas carpetas y estructura del proyecto

--- src/index.php
+++ src/index.php
@@ -1,16 +1,12 @@
 <?php
 
-phpinfo();
-
-
-
-
-
-
-
-
-
-
-
-?>
+$nombre = "Brandon";
+$edad = 19;
+$carrera = "Ingeniería en Sistemas ";
+$ciudad = "cartago";
+echo '<h1><strong>Mi presentacion Personal</strong></h1>';
+echo "mi nombre es $nombre, mi edad es $edad, mi carrera es $carrera, mi ciudad es $ciudad <br>";
+$semestre = "5to Semestre";
+$materia = "Programacion Movil";
+echo ", estoy en el $semestre, y mi materia favorita es $materia";
 
