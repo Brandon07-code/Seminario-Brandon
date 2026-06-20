d819e0af18077b6eae36f300341349e143c51802
Brandon07 <brandongiraldo8@gmail.com>
2026-06-20 10:36:20 -0500

    agregan nuevas carpetas y estructura del proyecto

--- src/array/index.php
+++ src/array/index.php
@@ -13,12 +13,13 @@ $productos = [
 
 echo "<h2>Lista de productos</h2>";
 echo "<ul>";
-$totalConIva = $total * 1.19;
+
 $total = 0;
 foreach ($productos as $nombre => $precio) {
     echo "<li>$nombre: $$precio</li>";
     $total += $precio;
 }
+$totalConIva = $total * 1.19;
 echo "</ul>";
 echo "<p><strong>Total sin IVA: $$total</strong></p>";
 echo "<p><strong>Total con IVA: $$totalConIva</strong></p>";
